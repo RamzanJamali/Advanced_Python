68a66c58fd32207ee5535b3896de475c0320fe51
ramzanjamali <ramanjamali.com>
2022-12-06 18:20:37 +0500

    Added unit testing functionality

--- Benchmarking_1.py
+++ Benchmarking_1.py
@@ -14,6 +14,7 @@ class ParticleSimulator:
     def evolve(self, dt):
         timestep = 0.00001
         nsteps = int(dt/timestep)
+        # nsteps = 1000 if dt = 0.01
 
         for i in range(nsteps):
             for p in self.particles:
@@ -66,7 +67,7 @@ def visualize(simulator):
     anim = animation.FuncAnimation(fig, animate, init_func=init, blit= True, interval = 10)
     plt.show()
 
-
+# To visualize the particles
 def test_visualize():
     particles = [
         Particle(0.3, 0.5, 1),
@@ -77,5 +78,33 @@ def test_visualize():
     simulator = ParticleSimulator(particles)
     visualize(simulator)
 
+# Testing if the calculations are accurate
+def test_evolve():
+    particles = [
+        Particle(0.3, 0.5, +1),
+        Particle(0.0, -0.5, -1),
+        Particle(-0.1, -0.4, +3)
+    ]
+    
+    simulator = ParticleSimulator(particles)
+
+    simulator.evolve(0.1)
+
+    p0, p1, p2 = particles
+
+    def fequal(a, b, eps= 1e-5):
+        return abs(a-b) < eps
+
+    assert fequal(p0.x, 0.210269)
+    assert fequal(p0.y, 0.543863)
+
+    assert fequal(p1.x, -0.099334)
+    assert fequal(p1.y, -0.490034)
+    
+    assert fequal(p2.x, 0.191358)
+    assert fequal(p2.y, -0.365227)
+    
+
 if __name__ == '__main__':
-    test_visualize()
+    # test_visualize()
+    test_evolve()
