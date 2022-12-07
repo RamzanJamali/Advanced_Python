eaebe2210b00cb81c66b9fd727945587abf237fd
ramzanjamali <ramanjamali.com>
2022-12-07 21:27:34 +0500

    Two separate files are created for testing

---
+++ test_simulator.py
@@ -0,0 +1,29 @@
+from simulator import Particle, ParticleSimulator
+
+def test_evolve():
+    particles = [
+        Particle(0.3, 0.5, 1),
+        Particle(0.0, -0.5, -1),
+        Particle(-0.1, -0.4, 3)
+    ]
+
+    simulator = ParticleSimulator(particles)
+
+    simulator.evolve(0.1)
+
+    p0, p1, p2 = particles
+
+    def fequal(a, b, eps=1e-5):
+        return abs(a-b) < eps
+
+    assert fequal(p0.x, 0.210269)
+    assert fequal(p0.y, 0.543863)
+
+    assert fequal(p1.x, -0.099334)
+    assert fequal(p1.y, -0.490034)
+    
+    assert fequal(p2.x, 0.191358)
+    assert fequal(p2.y, -0.365227)
+
+# To run test using pytest run this command
+# pytest test_simulator.py::test_evolve
