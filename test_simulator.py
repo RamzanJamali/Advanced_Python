9f9e4ea39efb0ecc59f6d71fb67ca8eb794f1228
ramzanjamali <ramanjamali.com>
2022-12-07 21:38:01 +0500

    tested program

--- test_simulator.py
+++ test_simulator.py
@@ -1,6 +1,6 @@
 from simulator import Particle, ParticleSimulator
 
-def test_evolve():
+def test_evolve(benchmark):
     particles = [
         Particle(0.3, 0.5, 1),
         Particle(0.0, -0.5, -1),
@@ -24,6 +24,8 @@ def test_evolve():
     
     assert fequal(p2.x, 0.191358)
     assert fequal(p2.y, -0.365227)
+
+    benchmark(simulator.evolve, 0.1)
 
 # To run test using pytest run this command
 # pytest test_simulator.py::test_evolve
