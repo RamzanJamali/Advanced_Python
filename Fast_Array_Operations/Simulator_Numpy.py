1d725ee427c5244f9246acf68560a941f55ce350
ramzanjamali <ramanjamali@gmail.com>
2022-12-22 22:13:14 +0500

    Previous simulator is modified using Numpy

---
+++ Fast_Array_Operations/Simulator_Numpy.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+# This program will simulate particles moving in a circular path at a constant speed. 
+# The purpose of this simulation is to improve performance of python program.
+class Particle:
+    def __init__(self, x, y, ang_vel):
+        self.x = x
+        self.y = y
+        self.ang_vel = ang_vel
+
+# This program will simulate particles moving in a circular path at a constant speed. 
+# The purpose of this simulation is to improve performance of python program.
+# To simulate the particles
+class ParticleSimulator:
+
+    def __init__(self, particles):
+        self.particles = particles
+
+    # The profile decorator is added to check time consumption line by line
+    # with line profiler
+    # To use line_profiler add @profile to function
+    # and use this command on command line:
+    # path_to_kernprof.exe -l -v path_to_simulator.py
+    # simplified: kernprof.exe -l -v simulator.py
+
+    #@profile
+    def evolve_numpy(self, dt):
+        timestep = 0.00001
+        nsteps = int(dt/timestep)
+        # nsteps = 1000 if dt = 0.01
+
+        r_i = np.array([[p.x, p.y] for p in self.particles])
+        ang_vel_i = np.array([p.ang_vel for p in self.particles])
+        
+        for i in range(nsteps):
+            
+            norm_ii = (r_i ** 2).sum(axis=1)
+            norm_i = np.sqrt(norm_ii)
+            v_i = r_i[:, [1, 0]]
+            v_i[:, 0] *= -1
+            v_i /= norm_i[:, np.newaxis]
+            d_i = timestep * ang_vel_i[:, np.newaxis] * v_i
+            r_i += d_i
+            
+            for i, p in enumerate(self.particles):
+                p.x, p.y = r_i[i]
+
+                
+    def evolve_python(self, dt):
+        timestep = 0.00001
+        nsteps = int(dt/timestep)
+        # nsteps = 1000 if dt = 0.01
+
+        for i in range(nsteps):
+            for p in self.particles:
+                # 1. Calculate the direction
+                norm = (p.x**2 + p.y**2)**0.5
+                v_x = -p.y/norm
+                v_y = p.x/norm
+
+                # 2. Calculate the displacement
+                d_x = timestep * p.ang_vel * v_x
+                d_y = timestep * p.ang_vel * v_y
+
+                p.x += d_x
+                p.y += d_y
+                # 3. repeat for all the timesteps 
+
+# This benchmark is to check how our code is performing
+from random import uniform
+
+def benchmark(npart=100, method='python'):
+    particles = [
+        Particle(uniform(-1.0, 1.0), uniform(-1.0, 1.0), uniform(-1.0, 1.0))
+        for i in range(npart)
+    ]
+
+    simulator = ParticleSimulator(particles)
+    
+    if method == 'python':
+        simulator.evolve_python(0.1)
+        
+    elif method == 'numpy':
+        simulator.evolve_numpy(0.1)
+
+if __name__ == '__main__':
+    benchmark(10, "python")
