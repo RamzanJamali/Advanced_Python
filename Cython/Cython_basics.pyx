a3b7883a975fd849155c0c2882a455f5beddd544
ramzanjamali <ramanjamali@gmail.com>
2023-01-16 21:48:15 +0500

    declaring typed functions in cython

--- Cython/Cython_basics.pyx
+++ Cython/Cython_basics.pyx
@@ -31,3 +31,35 @@ def example_python():
 
     return j
 # %timeit example_python()
+
+# creating objects in python
+cdef object a_py
+# both 'hello' and 1 are python objects
+a_py = 'hello'
+a_py = 1
+
+# changing type of variable in cython
+cdef int a = 0
+cdef double b
+b = <double> a
+
+
+# Defining functions 
+# with type information
+
+# first in python
+def max_python(int a, int b):
+    return a if a>b else b
+
+# now in cython (cannot be called in python)
+cdef int max_cython(int a, int b):
+    return a if a>b else b
+
+# declaring same function to call it from both python and cython
+# it generates two versions, one slower for python interpreter and second fast for c.
+cpdef int max_hybrid(int a, int b):
+    return a if a>b else b
+
+# writing small functions in inline so that it can be efficient when calling multiple times
+cdef inline int max_inline(int a, int b):
+    return a if a>b else b
