81bb609d23477c2a2d3fd55ff7fa0467754242be
ramzanjamali <ramanjamali@gmail.com>
2023-03-09 21:54:50 +0500

    Futures pattern added Futures pattern added to keep the track of the results of asynchronous calls.

---
+++ Implementing_Concurrency/example_04.py
@@ -0,0 +1,27 @@
+from concurrent.futures import Future
+import threading
+
+def network_request_async(number):
+    future = Future()
+    result = {
+        "success": True,
+        "result": number ** 2
+    }
+    timer = threading.Timer(1.0, lambda: future.set_result(result))
+    timer.start()
+    return future
+
+
+def fetch_square(number):
+    fut = network_request_async(number)
+
+    def on_done_future(future):
+        response = future.result()
+        if response["success"]:
+            print("Result is: {}".format(response["result"]))
+
+    fut.add_done_callback(on_done_future)
+
+print("start")
+fetch_square(2)
+print("end")
