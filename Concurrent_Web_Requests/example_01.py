6b3559e6460aaf7ccc55c8507fe6430c60ed9983
ramzanjamali <ramanjamali@gmail.com>
2023-05-04 23:18:02 +0500

    New chapter Concurrent_Web_Requests started

---
+++ Concurrent_Web_Requests/example_01.py
@@ -0,0 +1,14 @@
+import requests
+
+
+url = 'http://www.google.com'
+
+res = requests.get(url)
+
+print(res.status_code)
+print(res.headers)
+
+with open('google.html', 'w') as f:
+    f.write(res.text)
+
+print('Done.')
